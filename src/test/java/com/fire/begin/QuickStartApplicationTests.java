8c35459bf8c1a62509cdca6f66be2dd81f63a47d
981933231@qq.com <18164246818@hpz>
2018-10-03 22:58:40 +0800

    代码优化 支持list对象对比，后期指定key

--- src/test/java/com/fire/begin/QuickStartApplicationTests.java
+++ src/test/java/com/fire/begin/QuickStartApplicationTests.java
@@ -13,6 +13,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @Slf4j
@@ -34,7 +38,19 @@ public class QuickStartApplicationTests {
 
 
     @Test
-    public void testCompare() {
+    public void testSimpleObject() {
+        String a = "1";
+        String b = "2";
+
+        Diff diff = generalComparator.compare(a, b);
+
+        log.error(JsonUtils.toJson(diff));
+
+
+    }
+
+    @Test
+    public void testObjectCompare() {
         Student student = new Student();
         student.setId("1");
 
@@ -53,6 +69,33 @@ public class QuickStartApplicationTests {
         student1.setBook(book1);
 
         Diff diff = generalComparator.compare(student, student1);
+
+        log.error(JsonUtils.toJson(diff));
+    }
+
+
+    @Test
+    public void testSimpleList() {
+
+        List<String> list = Arrays.asList("1", "2");
+        List<String> list1 = Arrays.asList("1", "3");
+
+        Diff diff = generalComparator.compare(list, list1);
+
+        log.error(JsonUtils.toJson(diff));
+    }
+
+
+    @Test
+    public void testObjectList(){
+        Student student = new Student();
+        student.setId("1");
+
+
+        Student student1 = new Student();
+        student1.setId("2");
+
+        Diff diff = generalComparator.compare(Collections.singletonList(student), Collections.singletonList(student1));
 
         log.error(JsonUtils.toJson(diff));
     }
